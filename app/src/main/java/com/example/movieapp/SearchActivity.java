4cba8cec6b7b43eba6abfeeba47615cf67895651
meAhedonist8616 <sreyasgiri24704@gmail.com>
2024-12-19 06:53:21 +0530

    updation

--- app/src/main/java/com/example/movieapp/SearchActivity.java
+++ app/src/main/java/com/example/movieapp/SearchActivity.java
@@ -1,6 +1,9 @@
 package com.example.movieapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,20 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SearchActivity extends AppCompatActivity {
 
+    Button b1,b2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_search);
-
+        b1=(Button) findViewById(R.id.searchbut);
+        b2=(Button) findViewById(R.id.back2but);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(), MenuActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
